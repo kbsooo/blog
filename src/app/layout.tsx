3349edd83f3a793e2094368dd01d56989d7d8a42
Kbyungs <kbsoo0620@icloud.com>
2024-12-22 18:13:01 +0900

    home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "KBSOO",
+  title: "kbsoo/home/",
   description: "KBSOO's blog",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <div>
-      content
+    <div className="content">
+      <h4 className="intro-title">Welcome to KBSOO&apos;s page !</h4>
+      <p className="introduce">
+        I am a student at Myongji University, currently studying Computer Engineering.
+        My primary interests include deep learning, data science, and backend development.
+        I always enjoy learning and am eager to find opportunities where I can continue to grow and expand my skills in theses areas.
+      </p>
     </div>
   );
 }
